682e0fcb1a90ed6027d5d3768506ce953102a1b9
Mauricio calassara <mauriciocalassara0@gmail.com>
2021-02-19 15:22:44 -0300

    Adicionar rota para buscar emails

---
+++ api/emails_get.js
@@ -0,0 +1,17 @@
+const dbConn = require('../server/db.js');
+
+module.exports = (req, res) => {
+    try {
+        dbConn.query(`SELECT * FROM emails`, (erroDB, resDB) => {
+            if (erroDB) {
+                const erro = JSON.stringify({ cod: 502, mensagem: "Erro ao inserir no banco de dados!" });
+                throw new Error(erro);
+            }
+
+            res.status(200).send(resDB);
+        });
+    } catch ({ message }) {
+        const { cod, mensagem } = JSON.parse(message);
+        res.status(cod).send({ status: "Falha", mensagem });    
+    }
+}
--- server/rotas.js
+++ server/rotas.js
@@ -1,9 +1,12 @@
 const medidasPost = require('../api/medidas_post.js');
 const medidasGet = require('../api/medidas_get.js');
 const emailsPost = require('../api/emails_post.js');
+const emailsGet = require('../api/emails_get.js');
 
 module.exports = (app) => {
     app.post('/medidas', medidasPost);
     app.get('/medidas', medidasGet);
+
     app.post('/emails', emailsPost);
+    app.get('/emails', emailsGet);
 };
